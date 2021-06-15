dd4243ebc1dbdd2c04780b77a78090aa415042bc
徐行 <>
2021-06-15 23:23:28 +0800

    单个、多个拦截器的实现

---
+++ src/main/java/org/example/interceptor/MyInterceptor012.java
@@ -0,0 +1,30 @@
+package org.example.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyInterceptor012 implements HandlerInterceptor {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("MyInterceptor012=========preHandle");
+
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+        System.out.println("MyInterceptor012=================postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("MyInterceptor012+++++++++++afterCompletion");
+    }
+}
